2daed3ddf244ad228c270d8088d40f029f2955f8
Emiel Ergo <emiel.ergo@cloudinnovation.be>
2022-08-26 18:58:38 +0200

    Changes State messaging

--- src/rooms/MyRoom.ts
+++ src/rooms/MyRoom.ts
@@ -1,8 +1,9 @@
 import { Room, Client } from "@colyseus/core";
-import { Axie, Player, RaiderRoomState } from "./schema/RaiderRoomState";
+import { Axie, Bunker, Player, RaiderRoomState } from "./schema/RaiderRoomState";
 
 export class MyRoom extends Room<RaiderRoomState> {
     maxClients = 2;
+    private static counter = 0;
 
     onCreate(options: any) {
         console.log("MyRoom created.");
@@ -10,30 +11,34 @@ export class MyRoom extends Room<RaiderRoomState> {
 
         this.onMessage("updateAxie", (client, data) => {
             const player = this.state.players.get(client.sessionId);
-            const axie = player.axies.get(data["id"]);
-            axie.x = data["x"];
-            axie.y = data['y'];
-            axie.z = data["z"];
+            let axie = player.axies.get(data["id"]);
+            if (axie) {
+                axie.x = data["x"];
+                axie.y = data['y'];
+                axie.z = data["z"];
+            }
         });
 
         this.onMessage("insertAxie", (client, data) => {
-            // console.log("insert received -> ");
-            // console.debug(JSON.stringify(data));
+
             const player = this.state.players.get(client.sessionId);
-            const axie = new Axie();
-            axie.id = data["id"];
-            axie.skin = data["skin"];
-            axie.x = data["x"];
-            axie.y = data['y'];
-            axie.z = data["z"];
+            const axie = new Axie(data["id"], data["skin"], data["x"], data["y"], data["z"]);
+
             player.axies.set(axie.id, axie);
+
         });
 
         this.onMessage("removeAxie", (client, data) => {
-            // console.log("remove received -> ");
-            // console.debug(JSON.stringify(data));
+            let player = this.state.players.get(data["sessionId"]);
+            if (player) {
+                player.axies.delete(data["id"]);
+
+            }
+        });
+
+        this.onMessage("updateBunker", (client, data) => {
             const player = this.state.players.get(client.sessionId);
-            player.axies.delete(data["id"]);
+            player.bunker.hp = data["hp"];
         });
     }
 
@@ -41,13 +46,15 @@ export class MyRoom extends Room<RaiderRoomState> {
         console.log(client.sessionId, "joined!");
 
         // create Player instance
-        const player = new Player();
+        const player = new Player(MyRoom.counter % 2 + 1);
+        MyRoom.counter++;
+        player.bunker = new Bunker('bunker ' + player.number, 200, 15);
 
         // place axie in the map of axies by its sessionId
         // (client.sessionId is unique per connection!)
         this.state.players.set(client.sessionId, player);
 
-        console.log("new player =>", player.toJSON());
+        // console.log("new player =>", player.toJSON());
     }
 
     onLeave(client: Client, consented: boolean) {
--- src/rooms/schema/RaiderRoomState.ts
+++ src/rooms/schema/RaiderRoomState.ts
@@ -11,6 +11,14 @@ export class Axie extends Schema {
   // @type("Mesh") mesh: Mesh;
   // @type("Target") target: Target;
 
+  constructor(id: string, skin: string, x: number, y: number, z: number) {
+    super();
+    this.id = id;
+    this.skin = skin;
+    this.x = x;
+    this.y = y;
+    this.z = z;
+  }
 }
 
 export class Bunker extends Schema {
@@ -21,10 +29,23 @@ export class Bunker extends Schema {
   @type("number") x: number;
   @type("number") y: number;
   @type("number") z: number;
+
+  constructor(id: string, hp: number, range: number) {
+    super();
+    this.id = id;
+    this.hp = hp;
+    this.range = range;
+  }
 }
 export class Player extends Schema {
+  @type("number") number: number;
   @type({ map: Axie }) axies = new MapSchema<Axie>();
-  @type(Bunker) bunker = new Bunker();
+  @type(Bunker) bunker: Bunker;
+
+  constructor(counter: number) {
+    super();
+    this.number = counter;
+  }
 }
 
 
